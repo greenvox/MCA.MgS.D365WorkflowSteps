5bae4582ce04692bbf6517bc59458ba687f26a6f
Sarfraz Khan <Sarfraz.Khan@mcaconnect.com>
2020-12-14 20:11:29 -0600

    Add project files.

---
+++ MCA.MgS.D365WorkflowSteps/GetStringByValue.cs
@@ -0,0 +1,83 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk;
+
+namespace MCA.MgS.D365WorkflowSteps
+{
+    public class GetSingleValueByFetch : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Search Value")]
+        public InArgument<string> FieldValue { get; set; }
+
+        [RequiredArgument]
+        [Input("Search Field")]
+        public InArgument<string> FieldName { get; set; }
+
+        [RequiredArgument]
+        [Input("Search Entity")]
+        public InArgument<string> FieldEntity { get; set; }
+
+        [RequiredArgument]
+        [Input("Result Field")]
+        public InArgument<string> ResultField { get; set; }
+
+        [RequiredArgument]
+        [Input("Filter Condition")]
+        [Default("and")]
+        [ArgumentDescription(@"and OR or")]
+        public InArgument<string> Filter { get; set; }
+
+        [Input("Additional Fetch Conditions")]
+        [ArgumentDescription(@"<condition attribute='createdon' operator='last-x-hours' value='1' />")]
+        public InArgument<string> FetchFilters { get; set; }
+
+        [Input("Order By")]
+        [Default("createdon")]
+        public InArgument<string> OrderBy { get; set; }
+
+        [Input("Sort Order Descending")]
+        public InArgument<bool> DescendingOrder { get; set; }
+
+
+        [Output("Response")]
+        public OutArgument<string> Response { get; set; }
+
+        [Output("Status")]
+        public OutArgument<string> Status { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            var context = executionContext.GetExtension<IWorkflowContext>();
+            var serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            //var tracingService = executionContext.GetExtension<ITracingService>();
+
+            var fieldValue = FieldValue.Get(executionContext);
+            var fieldName = FieldName.Get(executionContext);
+            var entityName = FieldEntity.Get(executionContext);
+            var resultField = ResultField.Get(executionContext);
+            var fetchFilters = FetchFilters.Get(executionContext);
+            var orderBy = OrderBy.Get(executionContext);
+            var descendingOrder = DescendingOrder.Get(executionContext);
+            var order = string.Empty;
+            if (orderBy != null)
+            {
+                order = $@"<order attribute='{orderBy}' descending='{descendingOrder}' />";
+            }
+            var filter = Filter.Get(executionContext);
+            var status = "Failure";
+
+            var value = CrmUtility.GetStringByValueUsingFetch(service, entityName, resultField, filter, fieldName, fieldValue, fetchFilters, orderBy);
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                Response.Set(executionContext, value);
+
+                status = "Success";
+            }
+
+            Status.Set(executionContext, status);
+        }
+    }
+}
---
+++ MCA.MgS.D365WorkflowSteps/GetUsersOnPostMention.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using System.Activities;
+using System.Linq;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace MCA.MgS.D365WorkflowSteps
+{
+    public class GetUsersOnPostMention : CodeActivity
+    {
+        #region Exposed Workflow properties
+
+        [Input("Post")]
+        [RequiredArgument]
+        [ReferenceTarget("post")]
+        public InArgument<EntityReference> Post { get; set; }
+
+        [Output("Poster")]
+        public OutArgument<string> Poster { get; set; }
+            
+
+        [Output("Mentioned")]
+        public OutArgument<string> Response { get; set; }
+
+        #endregion Exposed Workflow properties
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            var context = executionContext.GetExtension<IWorkflowContext>();
+            var serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            var crmService = serviceFactory.CreateOrganizationService(context.UserId);
+            var tracingService = executionContext.GetExtension<ITracingService>();
+
+            try
+            {
+                var primaryEntityName = context.PrimaryEntityName;
+                var entity = context.PreEntityImages["PreBusinessEntity"];
+
+                if (primaryEntityName != "post")
+                {
+                    var ent = Post.Get(executionContext);
+                    entity = crmService.Retrieve(ent.LogicalName, ent.Id, new ColumnSet(true));
+                }
+                
+                var regardingObjectId = entity.GetEntityReference("regardingobjectid");
+                var poster = entity.GetEntityReference("createdby");
+                var text = entity.GetString("text");
+                var mentions = Utility.SplitStringOnBrackets(text);
+
+                var posterRec = crmService.Retrieve(poster.LogicalName, poster.Id, new ColumnSet("internalemailaddress", "systemuserid"));
+                var posterEmail = posterRec["internalemailaddress"].ToString();
+                
+                string emails = string.Join(",", mentions);
+
+                Response.Set(executionContext, emails);
+                Poster.Set(executionContext, posterRec);
+                
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(
+                    $"An error occurred in the {GetType()} plug-in. {Utility.HandleExceptions(ex)}");
+            }
+        }
+    }
+}
+;
